f26120be1bc5eb817eeabae7b4a6ae9f98434ea5
Dvd848 <Dvd848@users.noreply.github.com>
2017-09-09 18:40:13 +0300

    Add basic documentation

--- WavExtractor/CustomConfig.py
+++ WavExtractor/CustomConfig.py
@@ -1,9 +1,13 @@
 from functools import wraps
 
 class CustomConfigDecorators(object):
+    """
+    Decorators for custom configuration to handle book-keeping of custom values.
+    """
 
     @staticmethod
     def getter(func):
+        """Decorator to allow getters to read _attr for a given attr"""
         @wraps(func)
         def func_wrapper(self):
             if hasattr(self, '_' + func.__name__):
@@ -13,6 +17,11 @@ class CustomConfigDecorators(object):
 
     @staticmethod
     def setter(func):
+        """
+        Decorator to allow setters to write a custom value _attr for a given attr.
+        Decorated function should return the value to write, or raise an exception 
+        if the value is invalid.
+        """
         @wraps(func)
         def func_wrapper(self, value):
             return setattr(self, '_' + func.__name__, func(self, value))
--- WavExtractor/WavExtractor.py
+++ WavExtractor/WavExtractor.py
@@ -7,7 +7,11 @@ from ctypes import *
 from CustomConfig import *
 
 class mmapEx(mmap.mmap):
+    """Extend mmap to add additional functionality"""
+
     def finditer(self, str):
+        """Return an iterator yielding the start position over 
+        all non-overlapping matches for the str in mmap"""
         pos = 0
         while (pos >= 0):
             pos = self.find(str, pos)
@@ -18,6 +22,7 @@ class mmapEx(mmap.mmap):
 
         
 class WavHeaderStart(Structure):
+    """ The beginning of the WAV format header"""
     _pack_ = True
     _fields_ = [
         ("ChunkID",     c_char * 4),
@@ -27,17 +32,23 @@ class WavHeaderStart(Structure):
 
 
 class WavExtractor:
+    """Extracts WAV files from a given resource file"""
 
     ########################################
     # Config 
     ########################################
     class WavExtractorConfig(object):
+        """
+        Configuration for WavExtractor.
+        Getters return the default value, however if a setter exists, this value is overridable.
+        """
 
         ### -------------------------------------------
 
         @property
         @CustomConfigDecorators.getter
         def out_dir(self):
+            """output directory"""
             return os.path.join(os.getcwd(), "WAVs")
 
         @out_dir.setter
@@ -54,6 +65,7 @@ class WavExtractor:
         @property
         @CustomConfigDecorators.getter
         def out_file_name_prefix(self):
+            """Prefix for output file"""
             return "output_"
 
         @out_file_name_prefix.setter
@@ -69,7 +81,8 @@ class WavExtractor:
 
         @property
         @CustomConfigDecorators.getter
-        def out_file_name_suffix(self):
+        def out_file_extension(self):
+            """Extension for output file"""
             return ".wav"
 
         ### -------------------------------------------
@@ -77,6 +90,7 @@ class WavExtractor:
         @property
         @CustomConfigDecorators.getter
         def debug_skip_write(self):
+            """Skip writing the output file for debug purposes"""
             return True
 
         ### -------------------------------------------
@@ -84,6 +98,7 @@ class WavExtractor:
         @property
         @CustomConfigDecorators.getter
         def debug_enable_log(self):
+            """Log debug messages"""
             return True
 
         ### -------------------------------------------
@@ -102,6 +117,7 @@ class WavExtractor:
 
     @staticmethod
     def is_valid_wav_header(wavHeader):
+        """Perform basic tests on potential WAV header"""
         if (wavHeader.ChunkID != "RIFF"):
             return False
         if (wavHeader.Format != "WAVE"):
@@ -109,17 +125,29 @@ class WavExtractor:
         return True
 
     def log(self, msg):
+        """Log debug messages"""
         if not self.config.debug_enable_log:
             return
 
         print msg
 
     def getNextOutputPath(self):
+        """
+        Returns the absolute path for the next output file to be written to.
+        Places the output file in out_dir, naming it <out_file_name_prefix>_N.<out_file_extension>
+        where N is the first number for which a file does not already exist.
+        """
+
+        # Make sure the output directory exists, since path is absolute
         if not os.path.exists(self.config.out_dir):
             raise RuntimeError("Expecting {} to exist".format(self.config.out_dir))
+
+        # Create <out_file_name_prefix>_{}.<out_file_extension> format string
         base_name = self.config.out_file_name_prefix \
                     + "{}" \
-                    + self.config.out_file_name_suffix
+                    + self.config.out_file_extension
+
+        #Search for an N for which the base name doesn't already exist
         counter = 0
         while True:
             current_name = base_name.format(counter)
@@ -127,31 +155,39 @@ class WavExtractor:
             if (not os.path.exists(current_path)):
                 break
             counter += 1
+
         return current_path
 
 
     def Config(self, **kwargs):
+        """Allow the user to configure different options"""
         try:
             for key, val in kwargs.iteritems():
                 setattr(self.config, key, val)
         except AttributeError as e:
+            # Configuration doesn't exist or is read only
             msg = "Error: Can't set attribute {}!".format(key)
             self.log(msg)
             raise AttributeError(msg)
 
     def ResetConfig(self):
+        """Reset to default configuration"""
         self.config = self.WavExtractorConfig()
         self.config.out_file_name_prefix = os.path.splitext(os.path.basename(self.resource_path))[0] + "_"
 
 
     def Extract(self):
+        """Perform the extraction of WAV files from the resource file"""
         try:
             self.log("Will output files to {}".format(self.config.out_dir))
+
+            # Create output directory
             if not os.path.exists(self.config.out_dir):
                 os.makedirs(self.config.out_dir)
 
             with open(self.resource_path, "r+b") as input_f:
                 mm = mmapEx(input_f.fileno(), 0)
+
                 for riffStart in mm.finditer('RIFF'):
                     wavHeader = WavHeaderStart.from_buffer_copy(mm[riffStart:])
                     if not self.is_valid_wav_header(wavHeader):
@@ -159,10 +195,12 @@ class WavExtractor:
 
                     self.log("Found WAV file of size 0x{:02X} at offset 0x{:02X}".format(wavHeader.ChunkSize, riffStart))
 
+                    # Basic sanity of ChunkSize: Ensure it is contained in resource file
                     if riffStart + wavHeader.ChunkSize > mm.size():
                         self.log("\tWarning: File size {} is out of bounds".format(wavHeader.ChunkSize))
                         continue
 
+                    # Create output file
                     out_file_path = self.getNextOutputPath()
                     if not self.config.debug_skip_write:
                         with open(out_file_path, "wb") as output_f:
